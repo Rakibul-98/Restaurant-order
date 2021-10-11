ad09cb805d6e37c6a33aa28be7435f8a929b52a1
Rakibul-98 <rakibul.rupom2001@gmail.com>
2021-10-11 19:44:54 +0600

    item increasing decreasing function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function handleIncrease(item,isIncrease) {
+    const burgerCount=document.getElementById(item);
+    const burgerNumber=parseInt(burgerCount.value);
+    if(isIncrease=="true"){
+        const burgerNewCount=burgerNumber+1;
+        burgerCount.value=burgerNewCount;
+    }
+    else if(isIncrease=="false" && burgerNumber>0){
+        const burgerNewCount=burgerNumber-1;
+        burgerCount.value=burgerNewCount;
+    }
+}
+
+
+
+
 function handleOrder() {
     document.getElementById("menu-sec").style.display='none';
     document.getElementById("animation").style.display='block';
